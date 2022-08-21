6df4573dce6ff5ca4ae3baa381edf3dec37192c9
fayadehong <snackchina@163.com>
2022-08-21 12:43:54 +0800

    update index.js 注册接口路由 modify by zr

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 const { myBlogServer } = require("./server");
-
+const routes = require("./routes");
 // 创建server服务实例
 const server = new myBlogServer();
+
+// 注册路由接口
+for (const route of routes) {
+  server.setRoute(route.path, route.handler);
+}
 
 server.listen(3007);
