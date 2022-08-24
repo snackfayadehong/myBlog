f13f19caa7e567858c592df01c031ca286166fa9
snackchina@163.com <snackchina@163.com>
2022-08-24 17:37:26 +0800

    optimizing MusicPlayer.vue 优化音乐播放空间 modify by zr

--- src/components/Players/MusicPlayer.vue
+++ src/components/Players/MusicPlayer.vue
@@ -3,69 +3,50 @@
   <div class="music-container" :class="{ 'music-active-switch': offsetThreshold }">
     <div class="music-disk">
       <!--唱片图片-->
-      <img class="music-disk-picture" src="" alt="" />
+      <img class="music-disk-picture" :src="music.musicPic" alt="" />
     </div>
-
-    <!--进度条-->
-    <div class="music-slider">
-      <el-slider size="small" />
-    </div>
-
-    <!--按钮组-->
-    <div class="button-group">
-      <!--上一曲 按钮-->
-      <button class="play-button">
-        <icon-go-start theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-      </button>
-      <!--播放 按钮-->
-      <button class="play-button">
-        <icon-play-one theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-        <icon-pause theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-      </button>
-      <!--下一曲 按钮-->
-      <button class="play-button">
-        <icon-go-end theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-      </button>
-      <!--音量按钮-->
-      <div class="voice-container">
-        <button class="voice-button">
-          <icon-volume-notice theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-          <icon-volume-mute theme="outline" size="23" fill="#939393" :strokeWidth="3" strokeLinejoin="miter" strokeLinecap="butt" />
-        </button>
-        <div class="voice-slider">
-          <el-slider :max="1" :step="0.1" size="small" />
-        </div>
-      </div>
-    </div>
-
     <audio ref="musicAudio" class="audio-component" controls preload="auto" @canplay="changeDuration">
-      <source ref="musicSource" type="audio/mpeg" />
+      <el-icon ref="musicSource" type="audio/mpeg" />
     </audio>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MusicPlayer"
+  name: "MusicPlayer",
 };
 </script>
 <script setup>
 import { ref, onMounted } from "vue";
 import { getMusic } from "../../api/api.js";
 
-onMounted(() => {
-  getMusic();
+// 音乐信息
+const music = ref({});
+onMounted(async () => {
+  const { data: res } = await getMusic();
+  if (res.status !== 200) {
+    console.log("请求音乐数据失败");
+  } else {
+    music.value = res;
+  }
 });
 </script>
 
 <style lang="less" scoped>
+@keyframes musicPicRotating {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 .music-container {
   position: fixed;
   justify-content: center;
-  width: 280px;
-  height: 110px;
+  height: 100px;
   background-color: white;
-  border-radius: 15px;
+  border-radius: 50%;
   bottom: 15px;
   left: 10px;
   opacity: 1;
@@ -74,19 +55,20 @@ onMounted(() => {
 
 .music-disk {
   position: absolute;
-  width: 90px;
-  height: 90px;
-  left: 15px;
-  top: 10px;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
 }
 
 .music-disk-picture {
-  width: 90px;
-  height: 90px;
+  width: 100%;
+  height: 100%;
   border-radius: 50%;
   /*设置图片不可点击*/
   pointer-events: none;
+  transform: rotate(360edg);
+  transform-origin: center;
+  animation: musicPicRotating 5s linear infinite;
 }
 
 .music-disk-playing-style {
@@ -100,74 +82,6 @@ onMounted(() => {
   100% {
     transform: rotate(360deg);
   }
-}
-
-.button-group {
-  position: absolute;
-  width: 330px;
-  height: 38px;
-  left: 90px;
-  bottom: 13px;
-  margin-left: 10px;
-}
-
-.button-group > button {
-  margin-left: 10px;
-}
-
-.play-button {
-  float: left;
-  width: 31px;
-  height: 31px;
-  padding: 4px;
-  /*margin: 0px;*/
-  border: 0px;
-  border-radius: 50%;
-  margin: 7px 0px 0px 0px;
-}
-
-.voice-button {
-  float: left;
-  width: 31px;
-  height: 31px;
-  padding: 0px;
-  /*margin: 0px;*/
-  border: 0px;
-  border-radius: 50%;
-  margin: 7px 0px 0px 0px;
-  background-color: transparent;
-}
-
-.music-slider {
-  position: absolute;
-  top: 20px;
-  left: 120px;
-  width: 50%;
-}
-
-.voice-container {
-  float: left;
-  margin-left: 12px;
-  width: 31px;
-  height: 38px;
-  overflow: hidden !important;
-  transition: 0.5s;
-}
-
-.voice-container:hover {
-  width: 160px;
-}
-
-.voice-slider {
-  position: relative;
-  top: 2px;
-  right: -30px;
-  width: 90px;
-  height: 35px;
-  background-color: white;
-  border-radius: 10px;
-  padding: 0px 15px 0px 15px;
-  transition: 0.2s;
 }
 
 .audio-component {
