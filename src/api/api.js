7fbcff35a19903d8a369258caaae71168085d07b
snackchina@163.com <snackchina@163.com>
2022-08-24 17:36:29 +0800

    optimizing api.js 优化音乐调用 modify by zr

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import myAxios from "./myAxios.js";
+import { sample } from "lodash";
+//  音乐数组
+const musicArray = ["日落大道 梁博", "Shots", "17岁 刘德华", "县城 刘森"];
+const musicName = sample(musicArray);
 
 export const getMusic = async () => {
-  return await myAxios.post("/music/get", { music: "17岁" });
+  return await myAxios.post("/music/get", { music: musicName });
 };
