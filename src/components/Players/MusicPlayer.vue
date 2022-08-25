b29b007c5e564e4ee09dda172fe8cdd393441127
fayadehong <snackchina@163.com>
2022-08-25 22:15:26 +0800

    update App MusicPlayer.vue 优化音乐播放 modify by zr

--- src/components/Players/MusicPlayer.vue
+++ src/components/Players/MusicPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <!--音乐播放器-->
-  <div class="music-container">
+  <div class="music-container" @click="suspendMusic">
     <div class="music-disk">
       <!--唱片图片-->
       <img class="music-disk-picture" src="src/assets/musicPic.png" alt="" v-if="defaultPic" />
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  name: "MusicPlayer",
+  name: "MusicPlayer"
 };
 </script>
 <script setup>
@@ -20,10 +20,10 @@ import { ref, onMounted } from "vue";
 import { getMusic } from "../../api/api.js";
 
 const defaultPic = ref(true);
-const musicAudio = ref();
+const musicAudio = ref(null);
+
 // mounted
 const lister = onMounted(() => {
-  window.addEventListener("scroll", videoPlayer);
   window.addEventListener("click", videoPlayer);
 });
 
@@ -43,9 +43,12 @@ const videoPlayer = () => {
   if (musicAudio.value.paused === true) {
     musicAudio.value.play();
   } else {
-    window.removeEventListener("scroll", videoPlayer);
     window.removeEventListener("click", videoPlayer);
   }
+};
+const suspendMusic = () => {
+  if (musicAudio.value.paused === true) return;
+  musicAudio.value.pause();
 };
 </script>
 
@@ -81,6 +84,28 @@ const videoPlayer = () => {
   align-items: center;
   background-color: rgba(0, 0, 0, 0.7);
 }
+// 暂停样式
+.music-disk:hover::after {
+  content: "";
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  position: absolute;
+  background-image: url("/src/assets/suspend.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 40%;
+  background-color: rgba(89, 89, 89, 0.05);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(7.5px);
+  border: 0 solid rgba(255, 255, 255, 0.18);
+  box-shadow: rgba(14, 14, 14, 0.19) 0 6px 15px 0;
+  -webkit-box-shadow: rgba(14, 14, 14, 0.19) 0 6px 15px 0;
+  -webkit-border-radius: 50%;
+  color: rgba(128, 128, 128, 0.8);
+  //  开启点击事件
+  pointer-events: auto;
+}
 
 .music-disk-picture {
   width: 80%;
@@ -93,10 +118,6 @@ const videoPlayer = () => {
   animation: musicPicRotating 5s linear infinite;
   box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
     rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-}
-
-.music-disk-playing-style {
-  animation: music-disk-rotate 5s linear infinite;
 }
 
 @keyframes music-disk-rotate {
