855dbe5596fbc0617f8e05ab1b45aa2f998cc993
fayadehong <snackchina@163.com>
2022-08-28 19:47:54 +0800

    add db.js, config.js 数据库配置 modify by zr

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * 数据库默认配置
+ * @type {{dbConfig: {database: string, password: string, port: string, host: string, username: string}}}
+ */
+module.exports = {
+  dbConfig: {
+    database: "my_blog",
+    username: "root",
+    password: "fayadehong",
+    host: "localhost",
+    port: 3306
+  }
+};
---
+++ server/src/db.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql2");
+const { dbConfig } = require("./config/config");
+
+/**
+ * 数据库连接池
+ * @type {Pool} 连接池
+ * @author Ea
+ */
+const db = mysql.createPool({
+  host: dbConfig.host,
+  port: dbConfig.port,
+  database: dbConfig.database,
+  user: dbConfig.username,
+  password: dbConfig.password
+});
+
+// 测试连接
+// db.promise()
+//   .query("select 1 ")
+//   .then(([rows, fields]) => {
+//     console.log("Mysql client connected");
+//   })
+//   .catch(err => {
+//     console.error("Unable connect to Mysql", err);
+//   })
+//   .then(() => db.end());
+
+module.exports = db;
