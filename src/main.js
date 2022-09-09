b0a54ce2184a42db4d43b6a40452e25d82e128c5
snackchina@163.com <snackchina@163.com>
2022-09-09 16:58:03 +0800

    update hljs 增加markdown高亮显示 modify by zr

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import { createApp } from "vue";
 
+//导入highlight
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
+
 //导入ElementPlus
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-
 import "./style.less";
 
 import App from "./App.vue";
 const app = createApp(App);
+
+//创建highlight一个自定义全局指令
+app.directive("highlight", el => {
+  let blocks = el.querySelectorAll("pre code");
+  blocks.forEach(block => {
+    hljs.highlightBlock(block);
+  });
+});
 
 app.use(ElementPlus);
 
