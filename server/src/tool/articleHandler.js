38305d6b4888192c5fb866f36d703cb09c2a957f
fayadehong <snackchina@163.com>
2022-09-11 21:35:57 +0800

    update 完善文章处理 modify by zr

--- server/src/tool/articleHandler.js
+++ server/src/tool/articleHandler.js
@@ -1,13 +1,31 @@
-const db = require("../db");
-const marked = require("marked");
+const articlesModule = require("../model/my_articles");
 const fs = require("fs");
 const MyError = require("../exception");
-const { NOT_FOUND_ERROR_CODE, REQUEST_PARAMS_ERROR_CODE } = require("../exception/errorCode");
+const { NOT_FOUND_ERROR_CODE } = require("../exception/errorCode");
 
+/**
+ * 读取md文件内容
+ * @param path
+ * @author Ea
+ */
 function readeArticle(path) {
+  let a = "";
   if (!path) {
-    throw new MyError(REQUEST_PARAMS_ERROR_CODE);
+    throw new MyError(NOT_FOUND_ERROR_CODE);
+  } else {
+    fs.readFile(`${path}`, "utf-8", (err, data) => {
+      if (err) {
+        return "读取文件内容失败";
+      } else {
+        path(data);
+      }
+    });
   }
 }
-function addArticle() {}
+
+function addArticle(path) {
+  const res = readeArticle(path);
+  console.log(res);
+}
+addArticle("..\\..\\docs\\test.md");
 function changeArticle() {}
