524a1d433877b309f847e1f546ccac20eb960984
fayadehong <snackchina@163.com>
2022-09-27 17:10:36 +0800

    update 完善文章信息处理 查询、写入、修改 modify by zr

--- server/src/tool/articleHandler.js
+++ server/src/tool/articleHandler.js
@@ -1,5 +1,5 @@
-const articlesModule = require('../model/my_articles')
-const marked = require('marked')
+const articlesModule = require("../model/my_articles");
+const marked = require("marked");
 const fs = require("fs");
 const MyError = require("../exception");
 const { NOT_FOUND_ERROR_CODE } = require("../exception/errorCode");
@@ -13,7 +13,7 @@ marked.setOptions({
   sanitize: false,
   smartLists: true,
   smartypants: false
-});//基本设置
+}); //基本设置
 
 /**
  * 读取md文件内容
@@ -24,38 +24,101 @@ function readeArticle(path) {
   if (!path) {
     throw new MyError(NOT_FOUND_ERROR_CODE);
   } else {
-    return new Promise( (res,rej)=>{
+    return new Promise((res, rej) => {
       fs.readFile(`${path}`, "utf-8", (err, data) => {
         if (err) {
-          rej(err)
+          rej(err);
         } else {
-          res(data)
+          res(data);
         }
       });
-    })
+    });
   }
 }
 
+/**
+ * 查询文章
+ * @param name
+ * @returns {Promise<void>}
+ * @author Ea
+ */
+async function searchArticle(name) {
+  return await articlesModule.findOne({
+    attributes: ["id", "fileName"],
+    raw: true,
+    where: { fileName: name }
+  });
+}
 
-let pattern = /<h1(([\s\S])*?)<\/h1>/;
+let titlePattern = /<h1(([\s\S])*?)<\/h1>/; // 标题
+let fileNamePattern = /(?!.*\/).+/;
 let articleValue = {
-  title:'',
-  content:'',
-}
- function addArticle(path) {
-  readeArticle(path).then(r=>{
-    let res  =  marked.parse(r.toString())
-    articleValue.content = res;
-    // articleValue.title = pattern.exec(res)[0];
-    try {
-      articlesModule.create({title:articleValue.title,content:articleValue.content})
-    }catch (e) {
-      console.log(e);
-    }
-  }).catch(e=>{
-    console.log(e)
-  })
+  fileName: "",
+  title: "",
+  content: ""
+};
 
+/**
+ * add写入文件内容
+ * @param path
+ * @author Ea
+ */
+function addArticle(path) {
+  // 读取文件内容并转换为HTMl
+  readeArticle(path)
+    .then(r => {
+      let res = marked.parse(r.toString());
+      articleValue.content = res;
+      articleValue.title = titlePattern.exec(res)[0];
+      articleValue.fileName = fileNamePattern.exec(path)[0];
+      // 写入
+      try {
+        articlesModule
+          .create({
+            fileName: articleValue.fileName,
+            title: articleValue.title,
+            content: articleValue.content
+          })
+          .then(r => {
+            console.log(r);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
-addArticle("../../docs/test.md");
-function changeArticle() {}
+
+/**
+ * 修改文章信息
+ * @param path
+ * @returns {Promise<void>}
+ * @author Ea
+ */
+async function changeArticle(path) {
+  const fileName = await searchArticle(fileNamePattern.exec(path)[0]); // 查询修改前的文章ID
+  if (fileName) {
+    const res = marked.parse(await readeArticle(path)).toString(); // 转换修改后的文章内容为html
+    articleValue.title = titlePattern.exec(res)[0];
+    articleValue.content = res;
+    // 修改文章信息
+    const result = await articlesModule.update(
+      { title: articleValue.title, content: articleValue.content },
+      {
+        where: {
+          id: fileName.id
+        }
+      }
+    );
+    if (result[0] !== 1) {
+      console.log("修改文章信息失败");
+    }
+  }
+}
+
+module.exports = {
+  addArticle,
+  changeArticle
+};
